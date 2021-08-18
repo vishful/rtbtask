8e965bedbfd868079301d7f54c4ea3c4f1c1c26a
Vishnu R Pillai <65284952+vishful@users.noreply.github.com>
2021-08-18 10:51:51 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+document.getElementById('closeButton').addEventListener('click', function(e) {
+    e.preventDefault();
+    this.parentNode.style.display = 'none';
+}, false);
+
+
+document.getElementById('closeButton2').addEventListener('click', function(e) {
+    e.preventDefault();
+    this.parentNode.style.display = 'none';
+}, false);
+
+document.getElementById('closeButton3').addEventListener('click', function(e) {
+    e.preventDefault();
+    this.parentNode.style.display = 'none';
+}, false);
